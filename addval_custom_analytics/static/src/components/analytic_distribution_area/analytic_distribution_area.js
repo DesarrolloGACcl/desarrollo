4146ed2b8e956517a8bc4cfa9383bb6cd2103274
equirogag <equiroga@addval.cl>
2024-03-28 17:37:33 -0300

    test inherit

--- addval_custom_analytics/static/src/components/analytic_distribution_area/analytic_distribution_area.js
+++ addval_custom_analytics/static/src/components/analytic_distribution_area/analytic_distribution_area.js
@@ -1,21 +1,8 @@
 import { AnalyticDistribution } from "@analytic/components/analytic_distribution/analytic_distribution";
 import { registry } from "@web/core/registry";
-import { useService, useOwnedDialogs } from "@web/core/utils/hooks";
-import { evaluateExpr } from "@web/core/py_js/py";
-import { getNextTabableElement, getPreviousTabableElement } from "@web/core/utils/ui";
-import { usePosition } from "@web/core/position_hook";
-import { getActiveHotkey } from "@web/core/hotkeys/hotkey_service";
-import { shallowEqual } from "@web/core/utils/arrays";
-import { sprintf } from "@web/core/utils/strings";
-import { _lt } from "@web/core/l10n/translation";
-import { AutoComplete } from "@web/core/autocomplete/autocomplete";
 
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
-import { TagsList } from "@web/views/fields/many2many_tags/tags_list";
-import { useOpenMany2XRecord } from "@web/views/fields/relational_utils";
-import { parseFloat as oParseFloat } from "@web/views/fields/parsers";
-import { formatPercentage } from "@web/views/fields/formatters";
-import { SelectCreateDialog } from "@web/views/view_dialogs/select_create_dialog";
+import { _lt } from "@web/core/l10n/translation";
+
  
 export class AnalyticDistributionArea extends AnalyticDistribution {
     async fetchAllPlans(nextProps) {
@@ -24,4 +11,4 @@ export class AnalyticDistributionArea extends AnalyticDistribution {
     }
 }
  
-registry.category("fields").add("analytic_distribution_area", AnalyticDistributionArea);
+registry.category("fields").add("analytic_distribution", AnalyticDistributionArea);

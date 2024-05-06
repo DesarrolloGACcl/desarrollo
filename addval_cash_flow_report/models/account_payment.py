75e8f641502f04f841fa55741998302b60a5f9f3
equirogag <equiroga@addval.cl>
2024-05-06 17:43:30 -0400

    change the way to add de type from payment to move

--- addval_cash_flow_report/models/account_payment.py
+++ addval_cash_flow_report/models/account_payment.py
@@ -12,17 +12,3 @@ class AccountPayment(models.Model):
     def _onchange_partner_id(self):
         self.principal_account_id = self.partner_id.principal_account_id
         self.secondary_account_id = self.partner_id.secondary_account_id
-
-    def action_post(self):
-        ''' draft -> posted '''
-        self.move_id.principal_account_id = self.principal_account_id
-        self.move_id.secondary_account_id = self.secondary_account_id
-
-        self.move_id.line_ids[0].principal_account_id = self.move_id.principal_account_id
-        self.move_id.line_ids[0].secondary_account_id = self.move_id.secondary_account_id
-
-        self.move_id._post(soft=False)
-
-        self.filtered(
-            lambda pay: pay.is_internal_transfer and not pay.paired_internal_transfer_payment_id
-        )._create_paired_internal_transfer_payment()

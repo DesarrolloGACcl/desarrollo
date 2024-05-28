03c3b9f70cdb9a0370545b536416852a2001bdea
equirogag <equiroga@addval.cl>
2024-05-28 10:59:19 -0400

    update config fields

--- addval_rindegastos/models/res_company.py
+++ addval_rindegastos/models/res_company.py
@@ -1,17 +1,16 @@
 from odoo import models, fields
 
-
 class ResCompany(models.Model):
     _name = "res.company"
 
-    expense_account_id = fields.Many2one(
+    rindegastos_expense_account_id = fields.Many2one(
         'account.account',
         string = 'Cuenta de gastos',
         store = True,
         readonly=False,
     )
 
-    account_vendor_id = fields.Many2one(
+    rindegastos_account_vendor_id = fields.Many2one(
         'account.account',
         string = 'Cuenta proveedor',
         store = True,
@@ -24,13 +23,13 @@ class ResCompany(models.Model):
         readonly=False
     )
 
-    days_update = fields.Integer(
+    rindegastos_days_update = fields.Integer(
         string = 'Dias actualizar',
         store = True,
         readonly=False
     )
 
-    journal_id = fields.Many2one(
+    rindegastos_journal_id = fields.Many2one(
         'account.journal',
         string = 'Diario RindeGastos',
         store = True,
--- addval_rindegastos/models/res_config_settings.py
+++ addval_rindegastos/models/res_config_settings.py
@@ -4,17 +4,17 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    expense_account_id = fields.Many2one(
+    rindegastos_expense_account_id = fields.Many2one(
         'account.account',
-        related='company_id.expense_account_id',
+        related='company_id.rindegastos_expense_account_id',
         string = 'Cuenta de gastos',
         store = True,
         readonly=False,
     )
 
-    account_vendor_id = fields.Many2one(
+    rindegastos_account_vendor_id = fields.Many2one(
         'account.account',
-        related='company_id.account_vendor_id',
+        related='company_id.rindegastos_account_vendor_id',
         string = 'Cuenta proveedor',
         store = True,
         readonly=False
@@ -27,16 +27,16 @@ class ResConfigSettings(models.TransientModel):
         readonly=False
     )
 
-    days_update = fields.Integer(
-        related='company_id.days_update',
+    rindegastos_days_update = fields.Integer(
+        related='company_id.rindegastos_days_update',
         string = 'Dias actualizar',
         store = True,
         readonly=False
     )
 
-    journal_id = fields.Many2one(
+    rindegastos_journal_id = fields.Many2one(
         'account.journal',
-        related='company_id.journal_id',
+        related='company_id.rindegastos_journal_id',
         string = 'Diario RindeGastos',
         store = True,
         readonly=False

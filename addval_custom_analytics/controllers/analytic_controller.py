46af83ac031118d36604f0215f88cb185e68f89b
equirogag <equiroga@addval.cl>
2024-06-07 13:31:45 -0400

    crear proyecto si no existe

--- addval_custom_analytics/controllers/analytic_controller.py
+++ addval_custom_analytics/controllers/analytic_controller.py
@@ -15,28 +15,48 @@ class AnalyticApi(http.Controller):
     @http.route('/api/project', type='json', auth='public', methods=['POST'])
     def change_project_status(self, **kw):
 
-        expected_token = 'DLV86wKWGSjpsdhn'
-        provided_token = request.httprequest.headers.get('Authorization')
+        # expected_token = 'DLV86wKWGSjpsdhn'
+        # provided_token = request.httprequest.headers.get('Authorization')
 
-        if not provided_token:
-            return Response(json.dumps({"error": "Falta token"}), status=401, content_type='application/json')
+        # if not provided_token:
+        #     return Response(json.dumps({"error": "Falta token"}), status=401, content_type='application/json')
 
-        if provided_token != expected_token:
-            return Response(json.dumps({"error": "Unauthorized"}), status=401, content_type='application/json')
+        # if provided_token != expected_token:
+        #     return Response(json.dumps({"error": "Unauthorized"}), status=401, content_type='application/json')
         
         kw.get("project_code") #Código proyecto
+        kw.get("project_name") 
+        kw.get("partner_rut")
         kw.get("status") #(posibles estados "in_process", "invoicing”, "ended")
 
         project_analytic_plan = request.env['account.analytic.plan'].sudo().search([('id', '=', 1)])
 
         analytic_project = request.env['account.analytic.account'].sudo().search([('code', '=', kw.get("project_code")),
-                                                                                  ('plan_id', '=', project_analytic_plan.id)])               
+                                                                                  ('plan_id', '=', project_analytic_plan.id)])
         
-        if kw.get("status") == 'ended':
-            analytic_project.status = kw.get("status")
-            analytic_project.active = False
+        partner = request.env['res.partner'].sudo().search([('vat', '=', kw.get("partner_rut"))])
 
-            return 'El proyecto a sido archivado'
+        if not partner:
+            partner_id = None
+        else: 
+            partner_id = partner.id
+
+        if not analytic_project:
+            project = request.env['account.analytic.account'].sudo().create({
+                'code': kw.get("project_code"),
+                'name': kw.get("project_name"),
+                'partner_id': partner_id,
+                'plan_id': project_analytic_plan.id,
+                'status': kw.get("status") 
+            })
+
+            return 'Proyecto '+project.code+' '+project.name+' creado exitosamente'
         else:
-            analytic_project.status = kw.get("status")
-            return 'El proyecto a sido cambiado al estado: '+analytic_project.status
+            if kw.get("status") == 'ended':
+                analytic_project.status = kw.get("status")
+                analytic_project.active = False
+
+                return 'El proyecto a sido archivado'
+            else:
+                analytic_project.status = kw.get("status")
+                return 'El proyecto a sido cambiado al estado: '+analytic_project.status

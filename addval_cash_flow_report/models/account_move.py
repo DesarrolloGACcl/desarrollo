af928ecc51b69dd28863de808a74f1c8aaeeed56
equirogag <equiroga@addval.cl>
2024-06-13 18:21:44 -0400

    edit action and add logs

--- addval_cash_flow_report/models/account_move.py
+++ addval_cash_flow_report/models/account_move.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -10,10 +12,12 @@ class AccountMove(models.Model):
 
     def action_register_payment(self):
 
-        action = super().action_register_payment()
+        action = super(AccountMove, self).action_register_payment()
         action['context'].update({
             'principal_account_id': self.principal_account_id.id,
-            'secondary_account_id': self.secondary_account_id.id
+            'secondary_account_id': self.secondary_account_id.id,
+            'partner_id': self.partner_id.id
         })
+        _logger.warning('action: %s', action)
         
         return action

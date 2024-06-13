6dd96f5f7cbf67bc9f630076ee6b9d4c2918b3ec
equirogag <equiroga@addval.cl>
2024-06-13 18:24:50 -0400

    add partner id in context

--- addval_cash_flow_report/models/account_move.py
+++ addval_cash_flow_report/models/account_move.py
@@ -14,8 +14,8 @@ class AccountMove(models.Model):
 
         action = super(AccountMove, self).action_register_payment()
         action['context'].update({
-            'principal_account_id': self.principal_account_id.id,
-            'secondary_account_id': self.secondary_account_id.id,
+            'principal_account_id': self.partner_id.principal_account_id.id,
+            'secondary_account_id': self.partner_id.secondary_account_id.id,
             'partner_id': self.partner_id.id
         })
         _logger.warning('action: %s', action)

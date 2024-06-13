f9abec962c280c13996ead09a75dd5b42183bb05
equirogag <equiroga@addval.cl>
2024-06-13 18:10:24 -0400

    inherit action register payment

--- addval_cash_flow_report/models/account_move.py
+++ addval_cash_flow_report/models/account_move.py
@@ -7,3 +7,13 @@ class AccountMove(models.Model):
 
     principal_account_id = fields.Many2one('principal.account', string="Cuenta principal")
     secondary_account_id = fields.Many2one('secondary.account',string="Subcuenta")
+
+    def action_register_payment(self):
+
+        action = super().action_register_payment()
+        action['context'].update({
+            'principal_account_id': self.principal_account_id.id,
+            'secondary_account_id': self.secondary_account_id.id
+        })
+        
+        return action

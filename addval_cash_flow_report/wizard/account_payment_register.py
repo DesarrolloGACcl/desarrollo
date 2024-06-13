327b980ec8a405c88087d3745b286aec63ce08ff
equirogag <equiroga@addval.cl>
2024-06-13 12:02:14 -0400

    change function

--- addval_cash_flow_report/wizard/account_payment_register.py
+++ addval_cash_flow_report/wizard/account_payment_register.py
@@ -11,16 +11,17 @@ _logger = logging.getLogger(__name__)
 class AccountPaymentRegister(models.TransientModel):
     _inherit= 'account.payment.register'
 
-    principal_account_id = fields.Many2one('principal.account', string="Cuenta principal", compute="_compute_principal_account_id", precompute=True)
-    secondary_account_id = fields.Many2one('secondary.account', string="Subcuenta", compute="compute_secondary_account_id", precompute=True)
+    principal_account_id = fields.Many2one('principal.account', string="Cuenta principal", default=lambda self: self._get_default_principal_account())
+    secondary_account_id = fields.Many2one('secondary.account', string="Subcuenta", default=lambda self: self._get_default_secondary_account())
 
-    def _compute_principal_account_id(self):
-        for apr in self:
-            apr.principal_account_id = apr.partner_id.principal_account_id
 
-    def _compute_secondary_account_id(self):
-        for apr in self:
-            apr.secondary_account_id = apr.partner_id.secondary_account_id
+    @api.model
+    def _get_default_principal_account(self):
+        return self.env['res.partner'].browse(self.partner_id.id).principal_account_id.id
+    
+    @api.model
+    def _get_default_secondary_account(self):
+        return self.env['res.partner'].browse(self.partner_id.id).secondary_account_id.id
 
     def _post_payments(self, to_process, edit_mode=False):
         _logger.warning('ENTRO A LA FUNCION POST PAYMENT HEREDADA')

36c4456f57b4f2915dfab3c11ca1fd2e7565a36c
equirogag <equiroga@addval.cl>
2024-09-26 11:29:10 -0300

    add new data file

--- addval_cash_flow_report/__manifest__.py
+++ addval_cash_flow_report/__manifest__.py
@@ -19,6 +19,7 @@
 
     'data': [
         'data/data.xml',
+        'data/third_account_data.xml',
         #'data/custom_cash_flow_report.xml',
         #'data/account_report_actions.xml',
         'security/ir.model.access.csv',

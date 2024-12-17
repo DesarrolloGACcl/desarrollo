a766cc9435b31656f3c95ddc9065eac5a6eefed7
equirogag <equiroga@addval.cl>
2024-12-16 21:37:27 -0300

    update budget fields

--- invoice_approval/models/account_analaytic_account.py
+++ invoice_approval/models/account_analaytic_account.py
@@ -1,4 +1,6 @@
-from odoo import models, fields, api, _
+from odoo import models, fields, api, _, http
+from odoo.http import request
+import requests
 from odoo.tools.float_utils import float_round, float_compare
 from odoo.exceptions import UserError, ValidationError
 
@@ -10,3 +12,28 @@ class AccountAnalyticAccount(models.Model):
     _inherit = 'account.analytic.account'
 
     head_id = fields.Many2one(comodel_name='res.head', string='Jefatura')
+    initial_budget = fields.Float(string="Presupuesto inicial")
+    remaining_budget = fields.Float(string="Presupuesto restante")
+
+    def update_budgets(self):
+
+        url = "https://proyectos.gac.cl/endpoints/api.php/presupuestos?token=e4b8e12d1a2f4c8b9f3c0d2a8e7a6d4f"
+    
+        response = requests.request('GET', url)
+
+        project_data = response.json()
+        
+        for d in project_data['data']:
+            _logger.warning('DATA PROYECTO: %s', d)
+
+            project_code = str(d['proyecto']['codigo_proyecto'])
+
+            _logger.warning('CODIGO PROYECTO: %s', project_code)
+
+            project_analytic_account = self.env['account.analytic.account'].search([('code', '=', project_code)], limit=1)
+
+            if project_analytic_account:
+                _logger.warning('ENTRO IF')
+                project_budget = float(d['proyecto']['presupuesto_sdg'])
+                _logger.warning('PRESUPUESTO PROYECTO: %s', project_budget)
+                project_analytic_account.initial_budget = project_budget
--- invoice_approval/models/sale_order.py
+++ invoice_approval/models/sale_order.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields
+from odoo import api, models, fields, _
 from odoo.exceptions import UserError, ValidationError
 from pytz import timezone, UTC
 from datetime import datetime
@@ -13,3 +13,57 @@ class AccountMove(models.Model):
     pre_invoice_id = fields.Integer(string="Id pre-factura en sistema gestión")
     approve_date = fields.Date(string="Fecha de aprobación")
     is_approved = fields.Boolean(string="¿Está aprobada?", default=False)
+
+    initial_budget = fields.Float(string="Presupuesto inicial", compute="_compute_initial_budget")
+    remaining_budget = fields.Float(string="Presupuesto restante", compute="_compute_remaining_budget")
+
+    @api.depends('order_line.analytic_distribution')
+    def _compute_initial_budget(self):
+        for order in self:
+            initial_budget = 0.0
+            # Get first analytic account from order lines
+            for line in order.order_line:
+                if line.analytic_distribution:
+                    # Get first analytic account id from distribution
+                    analytic_id = list(line.analytic_distribution.keys())[0]
+                    analytic = self.env['account.analytic.account'].browse(int(analytic_id))
+                    initial_budget = analytic.initial_budget
+                    break
+            order.initial_budget = initial_budget
+
+    @api.depends('order_line.analytic_distribution') 
+    def _compute_remaining_budget(self):
+        for order in self:
+            remaining_budget = 0.0
+            # Get first analytic account from order lines
+            for line in order.order_line:
+                if line.analytic_distribution:
+                    # Get first analytic account id from distribution
+                    analytic_id = list(line.analytic_distribution.keys())[0]
+                    analytic = self.env['account.analytic.account'].browse(int(analytic_id))
+                    remaining_budget = analytic.remaining_budget
+                    break
+            order.remaining_budget = remaining_budget
+
+    def write(self, vals):
+        
+        res = super().write(vals)
+
+        # Check if state changed to sale (confirmed)
+        for order in self:
+            for line in order.order_line:
+                if line.analytic_distribution:
+                    analytic_id = list(line.analytic_distribution.keys())[0]
+                    analytic = self.env['account.analytic.account'].browse(int(analytic_id))
+                    
+                    # Update remaining budget
+                    new_remaining = analytic.remaining_budget - order.amount_total
+                    if new_remaining < 0:
+                        raise ValidationError(_('No hay suficiente presupuesto disponible en el proyecto.'))
+                        
+                    analytic.remaining_budget = new_remaining
+                    # Force recompute of remaining budget on order
+                    order._compute_remaining_budget()
+                    break
+
+        return res

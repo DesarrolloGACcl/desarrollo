18c13e50abb97367874f3e4ba2ed98a204901ecd
equirogag <equiroga@addval.cl>
2024-12-16 21:57:58 -0300

    fix second indice

--- invoice_approval/models/account_analaytic_account.py
+++ invoice_approval/models/account_analaytic_account.py
@@ -34,6 +34,6 @@ class AccountAnalyticAccount(models.Model):
 
             if project_analytic_account:
                 _logger.warning('ENTRO IF')
-                project_budget = float(d['proyecto']['presupuesto_sdg'])
+                project_budget = float(d['proyecto'][0]['presupuesto_sdg'])
                 _logger.warning('PRESUPUESTO PROYECTO: %s', project_budget)
                 project_analytic_account.initial_budget = project_budget

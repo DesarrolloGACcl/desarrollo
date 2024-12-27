57fac3e4f1cabb653175f0760e65443947013aa8
equirogag <equiroga@addval.cl>
2024-12-27 11:54:01 -0300

    fix variable name

--- invoice_approval/controllers/sale_order_api.py
+++ invoice_approval/controllers/sale_order_api.py
@@ -174,7 +174,7 @@ class MoveApi(http.Controller):
         invoice = request.env['account.move'].sudo().create(invoice_vals)
            
         # Vincular la factura con la orden
-        invoice.write({'invoice_line_ids': [(1, line.id, {'sale_line_ids': [(6, 0, order.order_line.ids)]}) for line in invoice.invoice_line_ids]})
+        invoice.write({'invoice_line_ids': [(1, line.id, {'sale_line_ids': [(6, 0, sale.order_line.ids)]}) for line in invoice.invoice_line_ids]})
 
         # Publicar la factura
         invoice.action_post()

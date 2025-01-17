172af19d99f0e978a290b874c8672136e2b315f5
equirogag <equiroga@addval.cl>
2025-01-17 02:38:45 -0300

    fix rate

--- invoice_approval/controllers/sale_order_api.py
+++ invoice_approval/controllers/sale_order_api.py
@@ -182,8 +182,8 @@ class MoveApi(http.Controller):
             ], order='name desc', limit=1)
             
             if rate:
-                tax_subtotal = tax_subtotal * rate.rate
-                no_tax_subtotal = no_tax_subtotal * rate.rate
+                tax_subtotal = tax_subtotal * rate.inverse_company_rate
+                no_tax_subtotal = no_tax_subtotal * rate.inverse_company_rate
             else:
                 return 'No se encontró tasa de conversión UF a CLP'
 
@@ -251,8 +251,7 @@ class MoveApi(http.Controller):
         invoice = request.env['account.move'].sudo().create(invoice_vals)
            
         # Vincular la factura con la orden
-        for inv_line in invoice.invoice_line_ids:
-            inv_line.sale_line_ids = sale.order_line
+        invoice.write({'invoice_line_ids': [(1, line.id, {'sale_line_ids': [(6, 0, sale.order_line.ids)]}) for line in invoice.invoice_line_ids]})
 
         # Publicar la factura
         invoice.action_post()
